2530804d062d80d1bc23606b398901f5da080958
Felipe Quinones <fdquinones@gmail.com>
2016-03-23 14:11:07 -0500

    cambios en portal

--- app/scripts/controllers/googleMap.js
+++ app/scripts/controllers/googleMap.js
@@ -10,7 +10,7 @@ angular
 
 function googleMapCtrl($scope, $timeout, $http) {
     $scope.mapOptions = {
-        zoom: 12,
+        zoom: 6,
         center: new google.maps.LatLng(-3.987861111111111, -79.19679722222223),
         // Style for Google Maps
         styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}],
